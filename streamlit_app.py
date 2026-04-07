629af921321886ef01b8a2ce76dbd27a50e919c0
Uncbatman <adammbori23@gmail.com>
2026-04-07 23:06:48 +0300

    imported huggingface

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 from pyairtable import Api
+from huggingface_hub import InferenceClient
 
 # ============================================================================
 # CONFIGURATION & SECRETS
